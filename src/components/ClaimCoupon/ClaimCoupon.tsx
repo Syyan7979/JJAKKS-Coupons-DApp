0579947d0b1a58e95a983ef007f7f32414cec43b
Aira <aealoveros@up.edu.ph>
2023-06-02 22:44:03 +0800

    Set card details: byte to text

--- src/components/ClaimCoupon/ClaimCoupon.tsx
+++ src/components/ClaimCoupon/ClaimCoupon.tsx
@@ -51,6 +51,20 @@ const ClaimCoupon = () => {
     return <div>Loading...</div>;
   }
 
+  const bytes2char = (bytes: Uint8Array) => {
+    const decoder = new TextDecoder();
+    return decoder.decode(bytes);
+  };
+
+  const hexToUint8Array = (hex: string): Uint8Array => {
+    const bytes = new Uint8Array(hex.length / 2);
+    for (let i = 0; i < hex.length; i += 2) {
+      bytes[i / 2] = parseInt(hex.substr(i, 2), 16);
+    }
+    return bytes;
+  };
+
+
   return (
     <div className='claim-coupon flex-wrap'>
       <div style={{ width: '100%' }}>
@@ -65,10 +79,10 @@ const ClaimCoupon = () => {
             key={index}
             index={index}
             image_src='https://umbrellacreative.com.au/wp-content/uploads/2020/01/hide-the-pain-harold-why-you-should-not-use-stock-photos-1024x683.jpg'
-            coupon_code={contract.coupon_code}
-            description={contract.description}
-            expiration_date={contract.expiration_date}
-            merchant={contract.merchant}
+            coupon_code={bytes2char(hexToUint8Array(contract.coupon_code))}
+            description={bytes2char(hexToUint8Array(contract.description))}
+            expiration_date={bytes2char(hexToUint8Array(contract.expiration_date))}
+            merchant={bytes2char(hexToUint8Array(contract.merchant))}
           />
         ))
       )}
