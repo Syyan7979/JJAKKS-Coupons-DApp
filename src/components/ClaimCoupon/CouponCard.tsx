920d34c3b9c1aece1fe494b419ee57f829a0f4ce
Aira <aealoveros@up.edu.ph>
2023-06-17 15:21:44 +0800

    Display Delete only when claimed

--- src/components/ClaimCoupon/CouponCard.tsx
+++ src/components/ClaimCoupon/CouponCard.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { PlusCircleOutlined, DeleteOutlined } from '@ant-design/icons';
 import { Avatar, Card } from 'antd';
-import { claim_coupon, access_contract_adress,burn } from '../../utils/operation';
+import { claim_coupon, access_contract_adress,burn,check_if_claimed } from '../../utils/operation';
 
 import './styles.css';
 import { color } from '@chakra-ui/react';
@@ -24,6 +24,21 @@ const CouponCard = ({
     expiration_date: string;
     disable: boolean;
 }) => {
+    const [claimed, setClaimed] = useState(false);
+
+    useEffect(() => {
+        const fetchClaimedStatus = async () => {
+            try {
+                const isClaimed = await check_if_claimed(index);
+                setClaimed(isClaimed);
+            } catch (error) {
+                console.error('Error fetching claimed status:', error);
+            }
+        };
+
+        fetchClaimedStatus();
+    }, [index]);
+
     const handleClaimCoupon = async () => {
         try {
             const contractAddress = await access_contract_adress(index); // Get the contract address using the provided function and the index prop
@@ -39,7 +54,7 @@ const CouponCard = ({
         }
     };
 
-    const HandleBurningCoupon = async () => {
+    const handleBurningCoupon = async () => {
         try {
             const contractAddress = await access_contract_adress(index); // Get the contract address using the provided function and the index prop
             console.log('Contract Address:', contractAddress); // Add this console log to check the contract address
@@ -60,11 +75,13 @@ const CouponCard = ({
         <Card
             style={{ width: 300, margin: '10px' }}
             cover={<img alt="example" src={image_src} />}
-            actions={disable? []:[
-                <PlusCircleOutlined key="claim" rev={undefined} onClick={handleClaimCoupon } />, // Add onClick handler to the PlusCircleOutlined icon
-                <DeleteOutlined key="burn" rev={undefined} onClick={HandleBurningCoupon } />, // 
-                // <EllipsisOutlined key="details" rev={undefined} label='See Details' />,
-            ]}
+            actions={
+                claimed
+                    ? [<DeleteOutlined key="burn" rev={undefined} onClick={handleBurningCoupon} />]
+                    : [
+                          <PlusCircleOutlined key="claim" rev={undefined} onClick={handleClaimCoupon} />,
+                      ]
+            }
         >
             <div className="cpn-coupon_code">{coupon_code}</div>
             <div className="cpn-merchant">{merchant}</div>
